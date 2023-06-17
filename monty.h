53166215607b8a57f8344ddb45749604ad1e23a6
H. Sabah <sabahhasbi00@gmail.com>
2023-06-17 04:03:52 -0700

    incomplete

--- monty.h
+++ monty.h
@@ -1,6 +1,9 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+#include <ctype.h>
+#include <stdio.h>
+
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -28,4 +31,5 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
 #endif
--- opcodes.c
+++ opcodes.c
@@ -1,9 +1,19 @@
 #include "monty.h"
 
-void push(stack_t **stack, unsigned int line_number)
+void push(stack_t **stack, unsigned int line_number, char **cmd)
 {
+	if (!cmd[1] || )
+	{
+		dprintf(2,  
+	}
 }
 
 void pall(stack_t **stack, unsigned int line_number)
 {
+	stack_t h;
+	int i;
+
+	(void) line_number;
+	for (i = 0, h = *stack; h; h = h->next, i++)
+		printf("%d\n", h->n);
 }
